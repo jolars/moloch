b65079d9d1f215bb34c15d9d600c565f57f89355
Johan Larsson <jolars@posteo.com>
2025-03-12 15:06:05 +0100

    chore: remove commented parts from nix flake

--- flake.nix
+++ flake.nix
@@ -51,32 +51,3 @@
       }
     );
 }
-# {
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-#     flake-utils.url = "github:numtide/flake-utils";
-#   };
-#
-#   outputs = {
-#     self,
-#     nixpkgs,
-#     flake-utils,
-#   }:
-#     flake-utils.lib.eachDefaultSystem (
-#       system: let
-#         pkgs = nixpkgs.legacyPackages.${system};
-#         texlive = pkgs.texlive.combine {inherit (pkgs.texlive) scheme-basic l3build;};
-#         l3build-wrapped = pkgs.writeShellScriptBin "l3build-wrapped" ''
-#           # NOTE: the trailing slash in TEXMF is required
-#           TEXMF="${texlive}/" ${texlive}/bin/l3build "$@"
-#         '';
-#       in {
-#         devShells.default = pkgs.mkShell {
-#           buildInputs = [
-#             texlive
-#             l3build-wrapped
-#           ];
-#         };
-#       }
-#     );
-# }
